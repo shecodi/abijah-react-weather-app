e5468792edd88f7cebae6319fef262de4c596ebf
God'swill Kutwane <gkutwane7@gmail.com>
2026-04-14 13:54:49 +0200

    just added trim next to city in url . also fixed the p line 61

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
+
+
+
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
 
  function searchWeather() {
    const apiKey = "efa9d0bfea1a536aaa9c77e4bedcbb45";
-   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.trim()}&appid=${apiKey}&units=metric`;
 
    fetch(url)
      .then((response) => response.json())
@@ -58,8 +61,8 @@ function toFahrenheit(celsius) {
           <p>{weather.condition}</p>
 
           <p>
-            Feels like:{Math.round(weather.temperature)}°C /{" "}
-            {toFahrenheit(weather.temperature)}°F{" "}
+            Feels like: {Math.round(weather.feelsLike)}°C /{" "}
+            {toFahrenheit(weather.feelsLike)}°F
           </p>
           <p>Humidity: {weather.humidity}%</p>
           <p>Wind: {weather.wind} m/s</p>
