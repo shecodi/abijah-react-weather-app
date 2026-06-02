0dec221fc82d97a76128f4d30db6296446ace0d2
God'swill Kutwane <gkutwane7@gmail.com>
2026-06-02 22:42:27 +0200

    Improved the styling and the generating of the buttons

--- ForecastChart.jsx
+++ ForecastChart.jsx
@@ -1,11 +1,18 @@
-import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from "recharts";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
 
 function ForecastChart({ data }) {
   const chartData = data.map((day) => ({
     day: new Date(day.day).toLocaleDateString("en-US", {
       weekday: "short",
     }),
-
     temp: Math.round(day.max),
   }));
 
@@ -13,21 +20,32 @@ function ForecastChart({ data }) {
     <div
       style={{
         width: "100%",
-        height: "100px",
-        marginTop: "30px",
+        height: "200px",
+        minHeight: "200px",
+        marginTop: "20px",
       }}
     >
-      <ResponsiveContainer>
+      <ResponsiveContainer width="100%" height="100%">
         <LineChart data={chartData}>
-          <XAxis dataKey="day" stroke="#3a2cbd" />
+          <CartesianGrid stroke="rgba(235, 213, 213, 0.87)" strokeDasharray="4 4" />
 
-          <Tooltip />
+          <XAxis dataKey="day" stroke="#ffffff" />
+
+          <YAxis stroke="#ffffff" />
+
+          <Tooltip
+            contentStyle={{
+              background: "rgba(255,255,255,0.9)",
+              borderRadius: "12px",
+              border: "none",
+            }}
+          />
 
           <Line
             type="monotone"
             dataKey="temp"
             stroke="#594bd4"
-            strokeWidth={5}
+            strokeWidth={3}
           />
         </LineChart>
       </ResponsiveContainer>
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 import WeatherCard from "../WeatherCard";
 import Forecast from "../Forecast";
@@ -10,14 +10,29 @@ import rainVideo from "./Videos/Rain.mp4";
 import sunnyVideo from "./Videos/Sunny.mp4";
 import nightVideo from "./Videos/Night.mp4";
 
-
-
-
 function App() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
   const [loading, setLoading] = useState(false);
   const [forecast, setForecast] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
+  const searchRef = useRef(null);
+
+ useEffect(() => {
+   function handleClickOutside(event) {
+     if (searchRef.current && !searchRef.current.contains(event.target)) {
+       setSuggestions([]);
+     }
+   }
+
+   document.addEventListener("mousedown", handleClickOutside);
+
+   return () => {
+     document.removeEventListener("mousedown", handleClickOutside);
+   };
+ }, []);
+
+
 
   function getDay(timestamp) {
     const date = new Date(timestamp * 1000);
@@ -27,47 +42,81 @@ function App() {
     });
   }
 
-  function handleSearch(searchCity = city) {
+  async function findCity(cityName) {
+    const response = await fetch(
+      `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=efa9d0bfea1a536aaa9c77e4bedcbb45`,
+    );
+
+    const data = await response.json();
+    console.log("Geocoding results:", data);
+    return data[0];
+  }
+
+  async function getCitySuggestions(cityName) {
+    if (cityName.length < 2) {
+      setSuggestions([]);
+      return;
+    }
+
+    const response = await fetch(
+      `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=efa9d0bfea1a536aaa9c77e4bedcbb45`,
+    );
+
+    const data = await response.json();
+
+    setSuggestions(data);
+  }
+
+  async function handleSearch(searchCity = city) {
     setLoading(true);
 
-    searchWeather(searchCity).then((data) => {
+    const location = await findCity(searchCity);
+
+    if (!location) {
+      alert("City not found ❌");
+      setLoading(false);
+      return;
+    }
+
+  
+    searchWeather(location.name).then((data) => {
       if (data.cod !== 200 && data.cod !== "200") {
         alert("City not found ❌");
         setLoading(false);
         return;
       }
 
- if (!data.main) {
-   alert("Weather failed to load");
-   return;
- }
+      if (!data.main) {
+        alert("Weather failed to load");
+        return;
+      }
 
-setWeather({
-  temperature: data.main.temp,
-  condition: data.weather[0].description,
-  city: data.name,
-  humidity: data.main.humidity,
-  wind: data.wind.speed,
-  feelsLike: data.main.feels_like,
-  time: data.dt,
-  icon: data.weather[0].icon,
+      setWeather({
+        temperature: data.main.temp,
+        condition: data.weather[0].description,
+        city: data.name,
+        humidity: data.main.humidity,
+        wind: data.wind.speed,
+        feelsLike: data.main.feels_like,
+        time: data.dt,
+        icon: data.weather[0].icon,
 
-  sunrise: data.sys.sunrise,
-  sunset: data.sys.sunset,
-});
-    
-     getForecast(data.coord.lat, data.coord.lon).then((forecastData) => {
-      setForecast(
-        forecastData.daily.time.map((day, index) => ({
-          day: day,
-          max: forecastData.daily.temperature_2m_max[index],
-          min: forecastData.daily.temperature_2m_min[index],
-          code: forecastData.daily.weathercode[index],
-        })),
-      );
-     });
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset,
+      });
 
-     setLoading(false); 
+      getForecast(data.coord.lat, data.coord.lon).then((forecastData) => {
+        setForecast(
+          forecastData.daily.time.map((day, index) => ({
+            day: day,
+            max: forecastData.daily.temperature_2m_max[index],
+            min: forecastData.daily.temperature_2m_min[index],
+            code: forecastData.daily.weathercode[index],
+          })),
+        );
+      });
+
+      setLoading(false);
     });
   }
 
@@ -77,38 +126,38 @@ setWeather({
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
 
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=efa9d0bfea1a536aaa9c77e4bedcbb45&units=metric`,
-      )
-        .then((response) => response.json())
-        .then((data) => {
-         setWeather({
-           temperature: data.main.temp,
-           condition: data.weather[0].description,
-           city: data.name,
-           humidity: data.main.humidity,
-           wind: data.wind.speed,
-           feelsLike: data.main.feels_like,
-           time: data.dt,
-           icon: data.weather[0].icon,
+        fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=efa9d0bfea1a536aaa9c77e4bedcbb45&units=metric`,
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            setWeather({
+              temperature: data.main.temp,
+              condition: data.weather[0].description,
+              city: data.name,
+              humidity: data.main.humidity,
+              wind: data.wind.speed,
+              feelsLike: data.main.feels_like,
+              time: data.dt,
+              icon: data.weather[0].icon,
 
-           sunrise: data.sys.sunrise,
-           sunset: data.sys.sunset,
-         });
+              sunrise: data.sys.sunrise,
+              sunset: data.sys.sunset,
+            });
 
-          return getForecast(lat, lon);
-        })
+            return getForecast(lat, lon);
+          })
 
-        .then((forecastData) => {
-          setForecast(
-            forecastData.daily.time.map((day, index) => ({
-              day: day,
-              max: forecastData.daily.temperature_2m_max[index],
-              min: forecastData.daily.temperature_2m_min[index],
-              code: forecastData.daily.weathercode[index],
-            })),
-          );
-        });
+          .then((forecastData) => {
+            setForecast(
+              forecastData.daily.time.map((day, index) => ({
+                day: day,
+                max: forecastData.daily.temperature_2m_max[index],
+                min: forecastData.daily.temperature_2m_min[index],
+                code: forecastData.daily.weathercode[index],
+              })),
+            );
+          });
       },
 
       () => {
@@ -126,29 +175,28 @@ setWeather({
     return Math.round((celsius * 9) / 5 + 32);
   }
 
-function getBackgroundVideo() {
-  if (!weather) return cloudsVideo;
+  function getBackgroundVideo() {
+    if (!weather) return cloudsVideo;
 
-  const condition = weather.condition.toLowerCase();
+    const condition = weather.condition.toLowerCase();
 
-  if (condition.includes("rain")) {
-    return rainVideo;
+    if (condition.includes("rain")) {
+      return rainVideo;
+    }
+
+    if (condition.includes("clear")) {
+      return sunnyVideo;
+    }
+
+    if (condition.includes("cloud")) {
+      return cloudsVideo;
+    }
+
+    return nightVideo;
   }
-
-  if (condition.includes("clear")) {
-    return sunnyVideo;
-  }
-
-  if (condition.includes("cloud")) {
-    return cloudsVideo;
-  }
-
-  return nightVideo;
-}
-
 
   return (
-    <div className="container">
+    <div className="app">
       <video
         src={getBackgroundVideo()}
         className="video-cover"
@@ -156,35 +204,68 @@ function getBackgroundVideo() {
         muted
         loop
       ></video>
+      <div className="container">
+        <div className="content-video">
+          <h2>Weather Forecast</h2>
 
-      <div className="content-video">
-        <h2>Weather Forecast</h2>
+          <div className="search-bar" ref={searchRef}>
+            <input
+              type="text"
+              placeholder="Enter city..."
+              value={city}
+              onChange={(e) => {
+                setCity(e.target.value);
 
-        <div className="search-bar">
-          <input
-            type="text"
-            placeholder="Enter city..."
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-          />
+                if (e.target.value.trim() === "") {
+                  setSuggestions([]);
+                  return;
+                }
 
-          <button onClick={() => handleSearch()}>Search Weather</button>
+                getCitySuggestions(e.target.value);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSearch();
+                  setSuggestions([]);
+                }
+              }}
+            />
+            {suggestions.length > 0 && (
+              <div className="suggestions">
+                {suggestions.map((place, index) => (
+                  <div
+                    key={index}
+                    className="suggestion-item"
+                    onClick={() => {
+                      setCity(place.name);
+                      setSuggestions([]);
+                      handleSearch(place.name);
+                    }}
+                  >
+                    {place.name}, {place.country}
+                  </div>
+                ))}
+              </div>
+            )}
+
+            <button onClick={() => handleSearch()}>Search Weather</button>
+          </div>
+
+          {loading && <div className="spinner"></div>}
+
+          {weather && (
+            <WeatherCard
+              weather={weather}
+              toFahrenheit={toFahrenheit}
+              formatTime={formatTime}
+              getDay={getDay}
+            />
+          )}
+
+          {forecast.length > 0 && <Forecast data={forecast} />}
+          {forecast.length > 0 && <ForecastChart data={forecast} />}
+          <Footer />
         </div>
-
-        {loading && <div className="spinner"></div>}
-
-        {weather && (
-          <WeatherCard
-            weather={weather}
-            toFahrenheit={toFahrenheit}
-            formatTime={formatTime}
-            getDay={getDay}
-          />
-        )}
-
-        {forecast.length > 0 && <Forecast data={forecast} />}
-        {forecast.length > 0 && <ForecastChart data={forecast} />}
-        <Footer />
       </div>
     </div>
   );
